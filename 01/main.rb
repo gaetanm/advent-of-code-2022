de97041d7f26a644d8f11074b6b8c9f5c2c863e1
Gaëtan Masson <im.gaetanmasson@gmail.com>
2022-12-01 15:35:57 +0100

    Add part 2

--- 01/main.rb
+++ 01/main.rb
@@ -6,10 +6,21 @@ class Test < Minitest::Test
     calories = File.read(path)
     assert most_calories(calories) == 24000
   end
+
+  def test_total_three_most_calories
+    path = File.join(File.dirname(__FILE__), 'test.txt')
+    calories = File.read(path)
+    assert total_three_most_calories(calories) == 45000
+  end
 end
 
 def most_calories(input)
   input.split("\n\n").map { |calories| calories.split("\n").map(&:to_i).sum }.max
 end
 
+def total_three_most_calories(input)
+  input.split("\n\n").map { |calories| calories.split("\n").map(&:to_i).sum }.max(3).sum
+end
+
 puts most_calories(File.read(File.join(File.dirname(__FILE__), 'input.txt')))
+puts total_three_most_calories(File.read(File.join(File.dirname(__FILE__), 'input.txt')))
