407fa0bc2fc1d05fbc3b80fd68c5443ffd19504f
Gaëtan Masson <im.gaetanmasson@gmail.com>
2022-12-04 16:12:42 +0100

    A bit of refactoring

--- 04/main.rb
+++ 04/main.rb
@@ -1,41 +1,31 @@
 require 'minitest/autorun'
 
 class Test < Minitest::Test
+  def input
+    File.read(File.join(File.dirname(__FILE__), 'test.txt'))
+  end
+
   def test_part1
-    path = File.join(File.dirname(__FILE__), 'test.txt')
-    input = File.read(path)
     assert part1(input) == 2
   end
 
   def test_part2
-    path = File.join(File.dirname(__FILE__), 'test.txt')
-    input = File.read(path)
     assert part2(input) == 4
   end
 end
 
 def part1(input)
-  elves_pairs = input.split("\n")
-  elves_pairs.map do |elves_pair|
-    assignments = sorted_assignments(elves_pair.split(","))
+  input.split("\n").map do |elves_pair|
+    assignments = elves_pair.split(",").map { |assignment| to_range(assignment.split("-")) }.sort_by {|a| a.size }
     assignments[1].cover?(assignments[0])
   end.count(true)
 end
 
 def part2(input)
-  elves_pairs = input.split("\n")
-  elves_pairs.map do |elves_pair|
-    assignments = to_range_array(elves_pair.split(","))
+  input.split("\n").map do |elves_pair|
+    assignments = elves_pair.split(",").map { |assignment| (to_range(assignment.split("-"))).to_a }
     (assignments[1] & (assignments[0])).empty?
   end.count(false)
-end
-
-def to_range_array(assignments)
-  [to_range(assignments[0].split("-")).to_a, to_range(assignments[1].split("-")).to_a]
-end
-
-def sorted_assignments(assignments)
-  [to_range(assignments[0].split("-")), to_range(assignments[1].split("-"))].sort_by {|a| a.size }
 end
 
 def to_range(assigment)
